946b24f627afbb30c45b90dfa6c95487e1cd495c
Simon Freyaldenhoven <simonfreyaldenhoven@gmail.com>
2023-04-05 10:25:11 -0400

    #12 functionality added, but arguments do not get passed correctly

--- analysis/source/main.py
+++ analysis/source/main.py
@@ -2,14 +2,14 @@ from lib.pyHelper import run_script
 
 def main():
 
-    #run_script("intuition.m", "illustration", program= "matlab") #Not yet supported
+    run_script("lowdim_figures.m", "illustration", program= "matlab") #Not yet supported
     #For future reference, the line below works if run from within \illustration.
     #Note that relative paths may need to be updated within matlab
     #subprocess.call("matlab -batch lowdim_figures")
 
-    run_script("simulate_data.py", "simulation", fresh_run=1)
+    #run_script("simulate_data.py", "simulation", fresh_run=1)
     
-    run_script("summary_stats.py", "estimation")
+    #run_script("summary_stats.py", "estimation")
     
     run_script("create_plots.R", "plot", program = "Rscript")
         
