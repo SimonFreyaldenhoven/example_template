bdf9c4da67fbf005e26c2627dff58be35835f779
jackbunge1 <jack.bunge@phil.frb.org>
2024-08-21 16:19:04 -0400

    Lln function (#33)
    * illustrate_lln function
    
    creates scatterplots of rolls X current mean
    
    * updating LaTeX
    
    updating paper with new scatterplots
    
    * updating creating_plots.R
    
    changing datastore folder from absolute to relative reference. Changed base R project 1 directory up to do this
    
    * updating latex/pdf
    
    pushing latex log and compiled pdf
    
    * Update time_log.txt
    
    * updating latex and paper
    
    adjusting paper formatting

--- analysis/source/plot/create_plots.R
+++ analysis/source/plot/create_plots.R
@@ -1,27 +1,33 @@
 pacman::p_load(tidyverse, here)
 
-here::i_am("analysis.Rproj")
+here::i_am("base.Rproj")
 
 main <- function() {
   
-  design_path <- here("source", "lib", "designs_to_run.csv")
+  design_path <- here("analysis", "source", "lib", "designs_to_run.csv")
   
   # Import list of designs as a character vector by pulling the first column
   designs <- read_csv(design_path, show_col_types = FALSE, col_names = FALSE) %>% pull(1)
-  data_folder <- here("output", "simulation")
-  out_folder <- here("output", "plot") 
+  data_folder <- here("analysis", "output", "simulation")
+  out_folder <- here("analysis", "output", "plot")
+  
+  # importing datastore folder:
+  datastore_folder <- here("datastore")
   
   for(design in designs){
     data_path <- here(data_folder, design)
     out_path <- here(out_folder, design)
+    
+    raw_data_path <- paste0(datastore_folder, "/", design, ".csv")
     
     # If out_path doesn't exist, create it
     dir.create(out_path, showWarnings = FALSE)
     
     plot_hist(data_path, out_path, "sum", "freq_table.csv", "hist.png", "blue")
     plot_hist(data_path, out_path, "sum", "freq_table.csv", "hist_red.png", "red")
+    
+    illustrate_lln(raw_data_path, out_path, "sum", paste0("lln_plot_", design, ".png"))
   }
-  
 }
 
 plot_hist <- function(data_path, out_path, var_name, in_file, out_file, fillcolor, height = 5, width = 6){
@@ -34,7 +40,6 @@ plot_hist <- function(data_path, out_path, var_name, in_file, out_file, fillcolo
         )
     )
     df <- read_csv(here(data_path, in_file),show_col_types = FALSE)
-
     plot <- df %>% 
       ggplot(aes(.data[[var_name]], count)) +
       geom_histogram(stat = "identity", fill=fillcolor) +
@@ -44,7 +49,33 @@ plot_hist <- function(data_path, out_path, var_name, in_file, out_file, fillcolo
   
 }
 
+illustrate_lln <- function(data_path, out_path, var_name, out_file, height = 5, width = 6) {
+  # Set plotting theme
+  theme_set(
+    theme_minimal() +
+      theme(
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank()
+      )
+  )
+  # data cleaning
+  df <- read_csv(data_path,show_col_types = FALSE) %>%
+    mutate("total" = `first die` + `second die`, "sum" = total)
+  for (i in 1:nrow(df)) {
+    if (i != 1) {
+      df$sum[i] = (df$sum[i - 1] + df$total[i])
+    }
+  }
+  df$sum_by_roll = df$sum / df$roll
+  
+  #plot
+  plot <- df %>%
+    ggplot(aes(x = roll, y = sum_by_roll)) +
+    geom_point()
+
+  ggsave(plot = plot, filename = here(out_path, out_file), h = height, w = width)
+  }
+
+
 
 main()
-
-
