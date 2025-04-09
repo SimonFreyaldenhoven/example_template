d35c75d046b5ec4b9292f751666f8e56615d7d7b
Braun <C1SZB02@rb.win.frb.org>
2025-04-09 15:37:46 -0400

    #35 Matlab, R, Python, Latex all working

--- analysis/source/main.py
+++ analysis/source/main.py
@@ -2,17 +2,16 @@ from lib.pyHelper import run_script
 
 def main():
 
-    # run_script("lowdim_figures.m", "illustration", program= "matlab", fresh_run=1) 
-    # # run_script("lowdim_figures.m", "analysis//output//illustration", program= "matlab", fresh_run=1)
+    run_script("lowdim_figures.m", "illustration", program= "matlab", fresh_run=1) 
 
-    # run_script("simulate_data.py", "simulation")
-    # run_script("summary_stats.py", "simulation")
+    run_script("simulate_data.py", "simulation")
+    run_script("summary_stats.py", "simulation")
 
     # run_script("auto.do", "application", program = "stata")
     
     run_script("create_plots.R", "plot", program = "Rscript")
     
-    # run_script("note.tex", "paper", program = "pdflatex")
+    run_script("note.tex", "paper", program = "pdflatex")
     
     return None
     
